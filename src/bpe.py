b22b99653d9d66c350687317462472814f634b63
tathi <tatsuya.hiraoka@nlp.c.titech.ac.jp>
2022-11-28 09:27:18 +0900

    fix args, output

--- src/bpe.py
+++ src/bpe.py
@@ -7,8 +7,8 @@ def getFreqBigram(data):
         for i in range(len(line)-1):
             w = line[i] + ('_',) + line[i+1]
             countDict[w] += 1
-    k,v = sorted(countDict.items(), key=lambda x:x[1])[-1]
-    return k
+    word, freq = sorted(countDict.items(), key=lambda x:x[1])[-1]
+    return word, freq
 
 def updateData(data, x):
     neoData = []
@@ -29,31 +29,93 @@ def updateData(data, x):
         neoData.append(neoLine)
     return neoData
 
-def main(inPath, outPath, limit):
-    data = [line.strip() for line in open(inPath)]
-    data = [[(w,) for w in line.split()] for line in data]
+def countWordSize(data):
+    return sum([len(line) for line in data])
 
+def countVocabSize(data):
+    return len({w for line in data for w in line})
+
+def merge(data, args):
+    mergeList = []
+    i = 0
     while True:
-        w = getFreqBigram(data)
-        data = updateData(data,w)
-        wordSize = sum([len(line) for line in data])
-        vocSize = len({w for line in data for w in line})
-        print('wordSize:', wordSize, w)
-        print('vocSize:', vocSize)
-        if wordSize<limit:
-            break
+        word, freq = getFreqBigram(data)
+        data = updateData(data, word)
+        mergeList.append((''.join(word), str(freq)))
+        wordSize = countWordSize(data)
+        vocabSize = countVocabSize(data)
+        print(vocabSize, len(mergeList))
+        i += 1
 
+        print('MERGE:', ''.join(word))
+        print('wordSize:', wordSize)
+        print('vocSize:', vocabSize)
+        if args.wordLimit is not None and wordSize <= args.wordLimit:
+            break
+        if args.vocabLimit is not None and vocabSize >= args.vocabLimit:
+            break
+        if args.numMerge is not None and i >= args.numMerge:
+            break
+    return data, mergeList
+
+def save(data, mergeList, args):
     data = [' '.join([w[0] for w in line]) for line in data]
-    f = open(outPath, 'w')
-    for line in data:
-        f.write(line+'\n')
-    f.close()
+    with open(args.outPath, 'w') as f:
+        for line in data:
+            f.write(line+'\n')
+    with open(args.outPath + '.mergeList', 'w') as f:
+        for line in mergeList:
+            f.write(' '.join(line)+'\n')
+
+def loadData(args):
+    data = [line.strip() for line in open(args.inPath)]
+    data = [[(w,) for w in line.split()] for line in data]
+    return data
+
+def checkArgs(args, data):
+    numWords = countWordSize(data)
+    numVocab = countVocabSize(data)
+
+    # only one limit can be accepted?
+    print("Merge tokens until ONE OF the following condition would be met:")
+    numConditions = 0
+    if args.wordLimit is not None:
+        print("WORD NUMBER -> %d" % args.wordLimit)
+        numConditions += 1
+        if args.wordLimit <= numWords:
+            print("CAUTION: The specified word limit is already satisfied.")
+            print("         Please set a value < current word size (%d)."%numWords)
+    if args.vocabLimit is not None:
+        print("VOCAB NUMBER -> %d" % args.vocabLimit)
+        numConditions += 1
+        if numVocab < args.vocabLimit:
+            print("CAUTION: The specified vocab limit is already satisfied.")
+            print("         Please set a value > current vocab size (%d)."%numVocab)
+    if args.numMerge is not None:
+        print("MERGE NUMBER -> %d" % args.numMerge)
+        numConditions += 1
+    if numConditions==0:
+        print("You must specify at least one of [--wordLimit, --vocabLimit, --numMerge].")
+        exit()
+
+def main(args):
+    data = loadData(args)
+    checkArgs(args, data)
+
+    data, mergeList = merge(data, args) # conditionをmergeに追記する、vocabをdumpできるようにする
+    save(data, mergeList, args) 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-i','--inPath',default='../data/alice_formatted.txt',type=str)
     parser.add_argument('-o','--outPath',default='../data/result_alice.txt',type=str)
-    parser.add_argument('-l','--limit',default=25000,type=int)
+    parser.add_argument('-wl','--wordLimit',default=None, type=int, 
+                        help='merge until number of words becomes lower than this limit')
+    parser.add_argument('-vl','--vocabLimit',default=None, type=int,
+                        help='merge until number of words in *vocab* reaches this limit')
+    parser.add_argument('-nm', '--numMerge', default=None, type=int,
+                        help='merge numMerge times')
     args = parser.parse_args()
-    main(args.inPath, args.outPath, args.limit)
+
+    main(args)
